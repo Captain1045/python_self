ab8a745cebc2d39fb85ddb3e11b18edab9df77d8
Captain1045 <amalnc055@gmail.com>
2021-02-08 14:42:43 +0530

    Update list.py

--- Basics/list.py
+++ Basics/list.py
@@ -1,7 +1,10 @@
 temp_list=[]
+temp_list1=['5','6','7','8']
+final=[]
 
 temp_list.extend('1')
 temp_list.extend('2')
 temp_list.extend('3')
 temp_list.extend('4')
-print(temp_list)
+final=temp_list+temp_list1
+print(final)
